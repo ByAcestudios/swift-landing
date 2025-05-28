0293dbc904a45e4fbc4d3df96edb24f78831bd66
ahamisi <ahamisivickzyjr@gmail.com>
2025-05-28 16:15:46 +0100

    Contact Form Upload

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -100,6 +100,8 @@ const ContactForm = () => {
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-swift-purple"
             />
           </div>
+
+          
           
           <div className="mb-6">
             <label htmlFor="message" className="block text-gray-700 font-medium mb-2">Message</label>
